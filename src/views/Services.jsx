4f5372264cc7afd783a5fad3d5fd0ef6cc99dd5b
mohdsubhankhan <khanmohdsubhan876@gmail.com>
2025-01-25 22:41:24 +0530

    update viteconfig

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
     // style={{backgroundImage: `url('https://i.pinimg.com/originals/b0/b1/f5/b0b1f5d33de00e3c21ad29bbba25e31b.gif')`}}>
     >
       <div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 px-4 pt-20"
+        className="max-w-7xl mx-auto sm:px-6 lg:px-8 px-4 pt-20"
         id="services"
       >
         <h2
