c0c12324ef58f5d1271fa049a944bd58bc8248d0
mohdsubhankhan <khanmohdsubhan876@gmail.com>
2025-01-25 17:36:50 +0530

    update project desc

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -19,10 +19,10 @@ const Projects = () => {
           What I Built
         </h4>
         <div className="mt-8 flex justify-between items-stretch flex-wrap">
-          <Card title="Calculator" desc="Hello this is 1st project Hello this is 1st project Hello this is 1st project" imagePath="calculator.png" liveLink="https://mohdsubhankhan.github.io/calculator-app/"/>
-          <Card title="QR Generator" desc="Hello this is 1st project Hello this is 1st project Hello this is 1st project" imagePath="qr.png" liveLink="https://mohdsubhankhan.github.io/Qr-generator-app/"/>
-          <Card title="Space" desc="Hello this is 1st project Hello this is 1st project Hello this is 1st project" imagePath="nasa.png" liveLink="https://mohdsubhankhan.github.io/space/"/>
-          <Card title="Drawing" desc="Hello this is 1st project Hello this is 1st Hello this is 1st project Hello this is 1st Hello this is 1st project Hello this is 1st  project Hello this is 1st project" imagePath="drawing.png" liveLink="https://mohdsubhankhan.github.io/drawing/"/>
+          <Card title="Calculator" desc="This is a simple, responsive calculator web app built using HTML, CSS, and JavaScript. It provides basic arithmetic operations like addition, subtraction, multiplication, and division." imagePath="calculator.png" liveLink="https://mohdsubhankhan.github.io/calculator-app/"/>
+          <Card title="QR Generator" desc="A simple and user-friendly web application to generate QR codes. This app allows users to input any text, URL, or contact information, and instantly generate a corresponding QR code." imagePath="qr.png" liveLink="https://mohdsubhankhan.github.io/Qr-generator-app/"/>
+          <Card title="Space" desc="Space Explorer: Your gateway to the solar system! Discover fascinating facts about the planets with this interactive app. Simply click on a planet to learn key details about its unique features, from Mercury's proximity to the Sun to Neptune's deep blue mysteries." imagePath="nasa.png" liveLink="https://mohdsubhankhan.github.io/space/"/>
+          <Card title="Drawing" desc="Dive into the world of art with our versatile drawing app! Designed for creators of all levels, it offers an array of tools like precision brushes, vibrant color palettes, and layer support to help you craft masterpieces effortlessly" imagePath="drawing.png" liveLink="https://mohdsubhankhan.github.io/drawing/"/>
           
         </div>
       </div>
