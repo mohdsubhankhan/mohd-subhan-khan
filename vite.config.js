390085d9c25c226fe9e601f4f2bf8d0855187db2
mohdsubhankhan <khanmohdsubhan876@gmail.com>
2025-01-27 15:34:12 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-// export default defineConfig({
-//   base: "/mohd-subhan-khan/",
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
 
-export default {
-  build: {
-    outDir: 'dist', // This should match the directory specified in Netlify
-  },
-};
+
+export default defineConfig({
+  base: "/mohd-subhan-khan/",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
+
+// export default {
+//   build: {
+//     outDir: 'dist', // This should match the directory specified in Netlify    
+//   },
+// };
